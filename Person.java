81772f48a4cd307b05f564da0e3762d8079c742e
sachii0224 <khalifahallah02@gmail.com>
2024-11-29 13:03:37 +0800

    The first code

---
+++ Person.java
@@ -0,0 +1,13 @@
+public class Person { 
+    public static void main(String[] args) {
+        String name = "Faqihah";
+        int age = 22;
+        double height = 1.57;
+        boolean isStudent = true;
+        long netWorth = 25000000L;
+        System.out.println("The student name is: " + name + " and age is: " + age);
+        System.out.println("The student height is: " + height + " and is a student: " + isStudent);
+        System.out.println("The student net worth is: RM" + netWorth);
+    }
+}
+
